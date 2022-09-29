707ac3b027effee4a38ab979cb8028b995a9d412
Maciej Jaszczuk <git@artprotect.pl>
2022-09-29 16:42:20 +0200

    api-news-complete

--- routes/apis.js
+++ routes/apis.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news');
+const defaultSort = -1;
+
+
+router.get('/', function(req, res, next) {
+  res.render('apis', { title: 'API' });
+});
+
+module.exports = router;
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('apis', { title: 'API' });
+router.get('/apinews', (req, res, next) => {
+  const search = req.query.search || '';
+  let sort = req.query.sort || defaultSort;
+
+  if(sort !==1 || sort !== -1) {
+    sort = defaultSort;
+  }
+
+  const findNews = News
+    .find({title: new RegExp(search.trim(), 'i')})
+    .sort({created: sort})
+    .select('title author');
+
+  findNews.exec((err, data) => {
+    res.json(data);
+  })
+});
+
+router.get('/apinews/:id', (req, res, next) => {
+  const id = req.params.id;
+  const findNews = News
+    .findById(id)
+    .select('title author'); //selektor pól które mają być pokazane w json
+
+  findNews.exec((err, data) => {
+    res.json(data);
+  })
 });
 
 module.exports = router;
